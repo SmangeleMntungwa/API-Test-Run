de4b8317bd02cfceedc03be175ab33e3d3786320
SmangeleMntungwa <mangie696@gmail.com>
2022-08-04 15:51:23 +0200

    adds

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -41,5 +41,4 @@ router.delete("/:id", (req, res) => {
 });
  
 
-
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,17 +1,119 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../lib/dbConnections");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+//const middleware = require("../middleware/auth");
 
-router.get("/", (req, res) => {
+// Register Route
+// The Route where Encryption starts
+
+// router.post("/register", (req, res) => {
+//  try { 
+
+//     let sql = "INSERT INTO users SET ?";
+//     // This is the body in requesting
+//     const {
+//       full_name,
+//       email,
+//       password,
+//       user_type,
+//       phone,
+//       country,
+//       billing_address,
+//       default_shipping_address,
+//     } = req.body;
+//     // The start of hashing / encryption
+    
+
+//     let user = {
+//       full_name,
+//       email,
+//       // We sending the hash value to be stored witin the table
+//       password:hash,
+//       user_type,
+//       phone,
+//       country,
+//       billing_address,
+//       default_shipping_address,
+//     };
+//     // connection to the database
+//     con.query(sql, user, (err, result) => {
+//       if (err) throw err;
+//       console.log(result);
+//       res.send(`User ${(user.full_name, user.email)} created successfully`);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+// Login
+router.post("/login", (req, res) => {
   try {
-    con.query("SELECT * FROM users", (err, result) => {
+    let sql = "SELECT * FROM users WHERE ?";
+    let user = {
+      email: req.body.email,
+    };
+    con.query(sql, user, async (err, result) => {
       if (err) throw err;
-      res.send(result);
+      if (result.length === 0) {
+        res.send("Email not found please register");
+      } else {
+        const isMatch = await bcrypt.compare(
+          req.body.password,
+          result[0].password
+        );
+        if (!isMatch) {
+          res.send("Password incorrect");
+        } else {
+          // The information the should be stored inside token
+          const payload = {
+            user: {
+              user_id: result[0].user_id,
+              full_name: result[0].full_name,
+              email: result[0].email,
+              user_type: result[0].user_type,
+              phone: result[0].phone,
+              country: result[0].country,
+              billing_address: result[0].billing_address,
+              default_shipping_address: result[0].default_shipping_address,
+            },
+          };
+          // Creating a token and setting expiry date
+          jwt.sign(
+            payload,
+            process.env.jwtSecret,
+            {
+              expiresIn: "365d",
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+        }
+      }
     });
   } catch (error) {
     console.log(error);
   }
 });
+// Verify
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorized Access!",
+      });
+    } else {
+      res.status(200);
+      res.send(decodedToken);
+    }
+  });
+});
+
+
 
 router.post('/', (req, res) => {
 const {email, password, full_name, billing_address, default_shipping_address, country, phone, user_type } = req.body
@@ -58,6 +160,7 @@ router.post("/register", (req, res) => {
     const {
       full_name,
       email,
+      password,
       user_type,
       phone,
       country,
@@ -65,7 +168,7 @@ router.post("/register", (req, res) => {
       default_shipping_address,
     } = req.body;
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    const hash = bcrypt.hashSync(password, salt);
     let user = {
       full_name,
       email,
@@ -86,6 +189,19 @@ router.post("/register", (req, res) => {
     console.log(error);
   }
 });
+
+
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 
 // Login
 router.post("/login", (req, res) => {
